fbf17fa6d9937f4fa0bc90ff42b78cb8fa8ecadd
Alexanderrubino05 <alexrubino05@gmail.com>
2025-04-01 00:17:23 +0200

    fix build

--- src/main.jsx
+++ src/main.jsx
@@ -1,22 +1,28 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
-import { createBrowserRouter, createRoutesFromElements, RouterProvider, Route, Routes } from "react-router-dom";
-import OverallDiv from "./Components/OverallDiv.jsx";
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  RouterProvider,
+  Route,
+  Routes,
+} from "react-router-dom";
+import OverallDiv from "./components/OverallDiv.jsx";
 import ProjectPage from "./HomePage/Sections/ProjectsPages/ProjectPage.jsx";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="*" element={<App />}>
       <Route index element={<OverallDiv />} />
-      <Route path="basketball-website" element={<ProjectPage />}/>
-      <Route path="bc-copenhagen-app" element={<ProjectPage />}/>
+      <Route path="basketball-website" element={<ProjectPage />} />
+      <Route path="bc-copenhagen-app" element={<ProjectPage />} />
     </Route>
   )
 );
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-     <RouterProvider router={router} />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
